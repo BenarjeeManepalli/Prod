90bad614181cc46c47f0134bfbd546372fcfd376
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-12 23:55:57 +0530

    this is another fourth script file

---
+++ Staging project/fifthscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS ("Mango" "Orange" "Grapes" "Pinaple" "Banana")
+echo  "the first value is ${FRUITS[0]}"
+echo  "the Second value is ${FRUITS[1]}"
+echo   "the Third value is ${FRUITS[2]}"
+echo   "the fourth value is ${FRUITS[3]}"
+echo  " all fruits are ${FRUITS[@]}"
