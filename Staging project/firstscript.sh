a34cc9bb174c76923e40152d13d9ef6a746f605f
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-12 23:26:10 +0530

    this is another new file

---
+++ Staging project/firstscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PERSON1=Ben
+PERSON2=Giri
+echo "Hello $PERSON2, How are you , How is going on"
+echo "Hello $PERSON1. I am good . as usual going on"
+echo " how is your project $PERSON2" 
